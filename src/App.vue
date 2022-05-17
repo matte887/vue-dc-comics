6b64bb07e435c897a844f3619e057feb26d4dfc6
matte887 <m.erroi92@gmail.com>
2022-05-17 15:18:06 +0200

    Implementata bozza header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <MyHeader />
+      <main>
+        <h2>--> Content goes here <--</h2>
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MyHeader from "./components/MyHeader.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    MyHeader,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "./style/commons.scss";
+@import "./style/variables.scss";
+
+.container {
+  width: 70%;
+  margin: 0 auto;
+}
+
+main {
+  width: 100%;
+  height: 150px;
+  background-color: $main-background-color;
+  h2 {
+    color: white;
+    text-align: center;
+    line-height: 150px;
+  }
 }
 </style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,98 @@
+<template>
+<header>
+    <div class="header__logo">
+        <img src="../assets/dc-logo.png" alt="">
+    </div>
+    <nav>
+        <ul class="header__menu">
+            <li v-for="(item, index) in headerLinks" v-key="index"><a href="">{{item.text}}</a></li>
+        </ul>
+    </nav>
+</header>
+</template>
+
+<script>
+export default {
+    name: "MyHeader",
+    data() {
+        return {
+            headerLinks: [
+                {
+                    ulr: "#",
+                    text: "Characters",
+                    active: false,
+                },
+                {
+                    ulr: "#",
+                    text: "Comics",
+                    active: true,
+                },
+                {
+                    ulr: "#",
+                    text: "Movies",
+                    active: false,
+                },
+                {
+                    ulr: "#",
+                    text: "TV",
+                    active: false,
+                },
+                {
+                    ulr: "#",
+                    text: "Games",
+                    active: false,
+                },
+                {
+                    ulr: "#",
+                    text: "Collectibles",
+                    active: false,
+                },
+                {
+                    ulr: "#",
+                    text: "Videos",
+                    active: false,
+                },
+                {
+                    ulr: "#",
+                    text: "Fans",
+                    active: false,
+                },
+                {
+                    ulr: "#",
+                    text: "News",
+                    active: false,
+                },
+                {
+                    ulr: "#",
+                    text: "Shop",
+                    active: false,
+                },
+            ],
+        };
+    },
+};
+</script>
+
+<style scoped lang="scss">
+@import "../style/variables.scss";
+header {
+    height: 150px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.header__menu {
+    list-style: none;
+    display: flex;
+}
+
+li {
+    text-transform: uppercase;
+    margin: 0 1rem;
+}
+
+a {
+    color: $header-text-color;
+}
+</style>
